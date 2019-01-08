3dcffe28298d11e881007012632ef2204578d4db
Alexander <xitman78@gmail.com>
2019-01-08 23:37:06 +0200

    Removed /lib from main package. Added metadata in root package.json Minor version changed 0.1.7

--- rx-forms/lib/index.d.ts
+++
@@ -1,6 +0,0 @@
-export { default as RxControl } from './RxControl';
-export { default as RxFormGroup } from './RxFormGroup';
-export { default as FormField } from './components/FormField';
-export { default as FormState } from './components/FormState';
-export { IControlState, IFieldInputHandlers } from './types';
-export { default as Validators } from './validators';
--- rx-forms/lib/validators.d.ts
+++
@@ -1,7 +0,0 @@
-import { RxValidator, RxValidatorCreator } from './types';
-import { Schema } from 'yup';
-declare const _default: {
-    required: RxValidatorCreator;
-    yup: (yupSchema: Schema<any>) => RxValidator;
-};
-export default _default;
